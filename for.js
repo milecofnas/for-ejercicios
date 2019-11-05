4e0190219e6787066a5a05c1c7752b9e817f302d
milecofnas <milecofnas@gmail.com>
2019-11-05 21:43:39 +0100

    first commit

---
+++ for.js
@@ -0,0 +1,16 @@
+//----EJERCICIO 1----//
+for (let i = 0; i <= 10; i++) {
+    console.log(i)
+}
+
+//----EJERCICIO 2----//
+for (let i = 100; i >= 0; i-=1) {
+    console.log(i)
+}
+
+//----EJERCICIO 3----//
+
+for(let i = 0; i <= 1000; i += 2) {
+    console.log(i)
+}
+
