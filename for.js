fa59dbcf28416c7c62ce95e50d37a61281355236
milecofnas <milecofnas@gmail.com>
2019-11-11 14:39:37 +0100

    ejercicios

--- for.js
+++ for.js
@@ -14,3 +14,44 @@ for(let i = 0; i <= 1000; i += 2) {
     console.log(i)
 }
 
+//----EJERCICIO 4----//
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`9 * ${i} = ${9 * i}`)
+}
+
+// ----EJERCICIO 5----//
+
+let suma = 0
+for (let i = 0; i <= 1000; i++) {
+suma = suma + i
+ console.log(suma)
+}
+
+// ----EJERCICIO 6----//
+
+let suma = 0
+for (let i = 1; i <= 1000; i+=2) {
+suma = suma + i
+console.log(suma)
+}
+
+----EJERCICIO 7----//
+
+let asterisco = "*";
+
+for (let i = 0; i <= 13; i++){
+    console.log(asterisco)
+    asterisco = asterisco + "*"
+}
+
+//----EJERCICIO 8----//
+
+let asterisco = "*"
+
+for (let i = 1; i <= 13; i += 2) {
+    console.log(asterisco)
+    asterisco = asterisco + "**"
+}
+
+
